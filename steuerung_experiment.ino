a66acd169d1c98e3e9797a71d392b70fb5090a28
GreenCap95 <52608125+GreenCap95@users.noreply.github.com>
2020-11-15 10:19:11 +0100

    style: add file for code for automatic experiment
    This file is supposed to be uploaded to the arduino before starting an
    automatic dataloging experiment

---
+++ steuerung_experiment.ino
@@ -0,0 +1,8 @@
+// ***Anforderungsliste an Arduinocode***
+
+// - Tür n-mal automatisch öffnen und schließen lassen
+// - je Türzyklus Zeitreihe für Druckwerte per USB rausschicken
+// - je Türzyklus Zeitreihe für Beschleunigungswerte (linear/rotatorisch) per USB rausschicken
+// - Tür manuell per Knopfdruck öffnen und schließen lassen
+// - Drucksensorwerte auslesen
+// - B-Sensorwerte auslesen
